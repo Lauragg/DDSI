38577f411fb9418b10f3b882b51587f85d4ac8d4
Lauragg <lauragogar@correo.ugr.es>
2018-12-14 18:53:39 +0100

    Continuando consultaPartidasPersonaje

--- p4/Main.py
+++ p4/Main.py
@@ -51,6 +51,18 @@ class Aplicacion(object):
         self.peticion_boton.grid(row = self.fila_actual, column = 0, columnspan = 2)
         self.fila_actual += 1
 
+        # Etiqueta ConsultarPartidasPersonaje
+        self.etiqueta_personaje = Label(ventana, text="Consultar partidas de un personaje.")
+        self.etiqueta_personaje.grid(row = self.fila_actual, column = 0, columnspan = 2)
+        self.fila_actual += 1
+
+        # Botón ConsultarPartidasPersonaje
+        self.peticion_boton = Button(ventana, text = "Elegir personaje")
+        self.peticion_boton.configure(command = self.query_personaje)
+        self.peticion_boton.grid(row = self.fila_actual, column = 0, columnspan = 2)
+        self.fila_actual += 1
+
+
         # Estado de la petición
         self.estado_peticion  = Label(ventana, text = '')
         self.estado_peticion.grid(row = self.fila_actual, column = 0, columnspan = 2)
@@ -177,6 +189,23 @@ class Aplicacion(object):
         else:
             self.PeticionAcabada()
         self.ventana_jugador.destroy()
+
+
+
+    def query_personaje(self):
+        self.ventana_personaje = Tk()
+        self.ventana_personaje.wm_title("Personaje")
+
+        #Entrada para el personaje
+        self.personaje_buscado = StringVar()
+        self.entrada_personaje = Entry(self.ventana_personaje,textvariable = self.personaje_buscado)
+        self.entrada_personaje.grid(row = 0,column = 0, columnspan = 2)
+
+        #Botón para el guardado
+        self.boton_personaje = Button(self.ventana_personaje, textvariable = self.personaje_buscado)
+        self.boton_personaje,configure(command = self.query_personaje_2)
+        self.boton_entrada.grid(row = 1, column = 0, columnspan = 2)
+
 
     def guardar_csv(self):
         if self.peticion_realizada:
--- p4/consultaPartidasPersonaje.py
+++ p4/consultaPartidasPersonaje.py
@@ -1,34 +1,75 @@
-import tkinter as tk
-import sys
+from tkinter import *
+import pymysql as mdb
+import pandas as pd
+import time
+from tkinter import messagebox
+import datetime
+import re
+import os
 
-class App(tk.Frame):
-    def __init__(self, ip, user, pswd, db, master=None):
-        super().__init__(master)
-        self.pack()
+class Aplicacion(object):
+    def __init__(self, ventana, ip, user, pswd, db):
 
-        #Inicializamos los datos para conectarnos la base de Datos
-        self.ip=ip
+        # Datos para conectarse a la base de datos
+        self.ip = ip
         self.user = user
-        self.pswd=pswd
-        self.db=db
+        self.pswd = pswd
+        self.db = db
 
-        self.peticion_realizada= False
+        self.peticion_realizada = False
         self.path_defecto="./data/partidasPersonaje.csv"
 
 
+        self.fila_actual = 0
 
+        # Etiqueta ConsultarPartidasPersonaje
+        self.etiqueta_personaje = Label(ventana, text="Consultar partidas de un personaje.")
+        self.etiqueta_personaje.grid(row = self.fila_actual, column = 0, columnspan = 2)
+        self.fila_actual += 1
+
+        # Botón ConsultarPartidasPersonaje
+        self.peticion_boton = Button(ventana, text = "Elegir personaje")
+        self.peticion_boton.configure(command = self.query_personaje)
+        self.peticion_boton.grid(row = self.fila_actual, column = 0, columnspan = 2)
+        self.fila_actual += 1
+
+
+    def query_personaje(self):
+        self.ventana_personaje = Tk()
+        self.ventana_personaje.wm_title("Personaje")
+
+        #Entrada para el personaje
+        self.personaje_buscado = StringVar()
+        self.entrada_personaje = Entry(self.ventana_personaje,textvariable = self.personaje_buscado)
+        self.entrada_personaje.grid(row = 0,column = 0, columnspan = 2)
+
+        #Botón para el guardado
+        self.boton_personaje = Button(self.ventana_personaje, textvariable = self.personaje_buscado)
+        self.boton_personaje,configure(command = self.query_personaje_2)
+        self.boton_entrada.grid(row = 1, column = 0, columnspan = 2)
+
+
+    def query_personaje_2(self):
+        #Lee el texto de la entrada
+        if self.personaje_buscado.get()== "":
+            messagebox.showinfo("Error", "No ha introducido ningún nombre de personaje")
+            self.personaje_buscado= "W1lly"
+
+        #Buscamos al personaje en el sistema.
+
+
+# Creamos la interfaz y se la pasamos a Aplicacion
 def main(argv):
-    # Creamos la aplicación
-    myapp = App(argv[1],argv[2],argv[3],argv[4])
+    # Argumentos de base de datos
+    ip = argv[1]
+    user = argv[2]
+    pswd = argv[3]
+    db = argv[4]
 
-    #
-    # Estos son los metodos que llaman a la clase controladora de la ventana.
-    #
-    myapp.master.title("Consulta de las partidas asociadas a un personaje.")
-    myapp.master.maxsize(1000, 400)
-
-    # Iniciamos el programa
-    myapp.mainloop()
+    # Creamos la ventana
+    ventana = Tk()
+    start = Aplicacion(ventana, ip, user, pswd, db)
+    ventana.mainloop()
 
 # Ejecutamos el programa
 if __name__ == "__main__":
